29e5acadde201ca5e332c75a11cd149a86870479
Double778 <596638119@qq.com>
2016-06-12 10:27:38 +0800

    房间爱迪噢批复结案

---
+++ Testdidi/app/src/main/java/com/zhao/testdidi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zhao.testdidi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 哈哈哈
+    }
+}
